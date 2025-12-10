c5ae691f9c20758e8b108d154faf982fa895aa9e
Tae.Garam <drasla@naver.com>
2025-12-10 11:11:36 +0900

    Home 코드 완성

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,41 @@
 import { Button, Card, Input, Label, Wrapper } from "../styles/styles.tsx";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router";
+
+type FormValues = {
+    userId: number;
+    keyword: string;
+};
 
 function Home() {
+    const navigate = useNavigate();
+
+    // const form = useForm<FormValues>();
+    const { register, handleSubmit } = useForm<FormValues>({
+        defaultValues: {
+            userId: 1,
+            keyword: "react",
+        },
+    });
+
+    const onSubmit = (data: FormValues) => {
+        const queryString = new URLSearchParams({
+            userId: data.userId.toString(),
+            keyword: data.keyword,
+        }).toString();
+        navigate(`/result?${queryString}`);
+    };
+
     return (
         <Wrapper>
             <Card>
                 <h2>종합연습</h2>
 
-                <form>
+                <form onSubmit={handleSubmit(onSubmit)}>
                     <Label>User ID</Label>
-                    <Input type={"number"} />
+                    <Input {...register("userId", { required: true })} type={"number"} />
                     <Label>Keyword</Label>
-                    <Input />
+                    <Input {...register("keyword", { required: true })} />
                     <Button>조회하기</Button>
                 </form>
             </Card>
