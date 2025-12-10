7e2bddeed6835b693bd7a431719e7ff765b0233b
Tae.Garam <drasla@naver.com>
2025-12-10 11:55:13 +0900

    테마 적용 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,27 @@
 import GlobalStyle from "./styles/GlobalStyle.ts";
 import { ThemeProvider } from "styled-components";
-import { LightTheme } from "./styles/themes.ts";
+import { DarkTheme, LightTheme } from "./styles/themes.ts";
 import { BrowserRouter, Route, Routes } from "react-router";
 import Home from "./pages/Home.tsx";
 import Result from "./pages/Result.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
 
 const client = new QueryClient();
 
 function App() {
+    const [isDark, setIsDark] = useState(false);
+
     return (
-        <ThemeProvider theme={LightTheme}>
+        <ThemeProvider theme={isDark ? DarkTheme : LightTheme}>
             <GlobalStyle />
             <QueryClientProvider client={client}>
                 <BrowserRouter>
                     <Routes>
-                        <Route path={"/"} element={<Home />} />
+                        <Route
+                            path={"/"}
+                            element={<Home isDark={isDark} setIsDark={setIsDark} />}
+                        />
                         <Route path={"/result"} element={<Result />} />
                     </Routes>
                 </BrowserRouter>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,20 @@
 import { Button, Card, Input, Label, Wrapper } from "../styles/styles.tsx";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router";
+import type { Dispatch, SetStateAction } from "react";
+import { FaMoon, FaSun } from "react-icons/fa";
 
 type FormValues = {
     userId: number;
     keyword: string;
 };
 
-function Home() {
+type Props = {
+    isDark: boolean;
+    setIsDark: Dispatch<SetStateAction<boolean>>;
+};
+
+function Home({ isDark, setIsDark }: Props) {
     const navigate = useNavigate();
 
     // const form = useForm<FormValues>();
@@ -29,7 +36,37 @@ function Home() {
     return (
         <Wrapper>
             <Card>
-                <h2>종합연습</h2>
+                <div
+                    style={{
+                        display: "flex",
+                        justifyContent: "space-between",
+                        alignItems: "center",
+                        marginBottom: "20px",
+                    }}>
+                    <h2>종합연습</h2>
+                    <button
+                        style={{
+                            padding: "6px 10px",
+                            borderRadius: 6,
+                            display: "flex",
+                            alignItems: "center",
+                        }}
+                        onClick={() => setIsDark(!isDark)}>
+                        {isDark ? (
+                            <FaMoon size={20}
+                            style={{
+                                color: "skyblue"
+                            }}/>
+                        ) : (
+                            <FaSun
+                                size={20}
+                                style={{
+                                    color: "orange",
+                                }}
+                            />
+                        )}
+                    </button>
+                </div>
 
                 <form onSubmit={handleSubmit(onSubmit)}>
                     <Label>User ID</Label>
